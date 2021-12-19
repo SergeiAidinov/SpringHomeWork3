6e8e4df808cce5d54ff573a64749eb0437e05727
SergeiAidinov <incoming34@yandex.ru>
2021-12-19 12:48:46 +0300

    For code review

--- src/test/java/ru/yandex/incoming34/controller/Controller.java
+++ src/test/java/ru/yandex/incoming34/controller/Controller.java
@@ -1,23 +1,21 @@
 package ru.yandex.incoming34.controller;
 
-import java.io.IOException;
-import java.util.Map;
 import java.util.UUID;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import ru.yandex.incoming34.models.Product;
 import ru.yandex.incoming34.service.Service;
 
@@ -25,18 +23,41 @@ import ru.yandex.incoming34.service.Service;
 @RestController
 @RequestMapping("/main")
 public class Controller {
-	
+	ObjectMapper objectMapper = new ObjectMapper();
 	Service service;
-	
+
 	@Autowired
 	public Controller(Service service) {
 		this.service = service;
 	}
-	
-	//@ResponseBody
+
 	@RequestMapping(path = "/show", method = RequestMethod.GET)
-	public JsonNode showProducts() throws JsonProcessingException, IOException, JSONException {
-		return  service.getProducts();
+	public ResponseEntity<JsonNode> showProducts() {
+		JsonNode jsonNode = objectMapper.createObjectNode();
+		try {
+			jsonNode = service.getProducts();
+		} catch (JsonProcessingException jsonProcessingException) {
+			return new ResponseEntity<JsonNode>(jsonNode, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return new ResponseEntity<JsonNode>(jsonNode, HttpStatus.OK);
 	}
+
+	@RequestMapping(path = "/addProduct", method = RequestMethod.PUT)
+	public HttpStatus putProduct(@RequestBody Product product) {
+		service.putProduct(product);
+		return HttpStatus.OK;
+	}
+	
+	@RequestMapping(path = "/getProductById", method = RequestMethod.GET)
+	public ResponseEntity<JsonNode> getProduct(@RequestParam UUID id) {
+		JsonNode jsonNode = objectMapper.createObjectNode();
+		try {
+			jsonNode = service.getProduct(id);
+		} catch (JsonProcessingException jsonProcessingException) {
+			return new ResponseEntity<JsonNode>(jsonNode, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return new ResponseEntity<JsonNode>(jsonNode, HttpStatus.OK);
+	}
+	
 
 }
